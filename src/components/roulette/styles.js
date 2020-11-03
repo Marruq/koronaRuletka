bcf6395e7f58c9a04aecbd58fe7b3c17bc6a2333
MarekG <marruq89@gmail.com>
2020-11-03 14:03:49 +0100

    styled components animacje

--- src/components/roulette/styles.js
+++ src/components/roulette/styles.js
@@ -1,10 +1,29 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 
+const resizeAnimation = keyframes`
+    0% {
+        transform: scale(1);
+    }
+    
+    50% {
+        transform: scale(1.2)
+    }
+    
+    100% {
+        transform: scale(1);
+    }
+`
 
 
 export const Score = styled.h1`
     font-size: ${props => props.result ? "60px" : "30px"};
     margin: 60px 0;
     color: ${props => props.result ? "#ffb142" : "white"};
+    
+    ${props => props.result && css`
+
+    animation: ${resizeAnimation} 2s linear infinite;
+  `}
+     
 `
